df2cf8e0c5c5bca815b51522b14811504277874b
nhdm-apps <kakotanjugjavlja@gmail.com>
2026-03-20 11:32:33 +0000

    finalna verzija - 01

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,32 +1,18 @@
-export const planiraniMeseci = [
-    {
-      mesec: "APRIL",
-      stavke: [
-        { datum: "1.apr", tip: "uplata", iznos: 41000, opis: "35.000 + 6.000 (doplata)", kategorija: "Druge uplate" },
-        { datum: "1.apr", tip: "isplata", iznos: 41000, opis: "Kirija", kategorija: "Kirija" },
-        { datum: "13.apr", tip: "uplata", iznos: 16000, opis: "WOLT", kategorija: "Wolt" },
-        { datum: "15.apr", tip: "uplata", iznos: 46000, opis: "NBS", kategorija: "NBS" },
-        { datum: "15.apr", tip: "isplata", iznos: 17000, opis: "Kredit", kategorija: "Kredit" },
-        { datum: "15.apr", tip: "isplata", iznos: 27225, opis: "Računi", kategorija: "Računi" },
-        { datum: "28.apr", tip: "uplata", iznos: 16000, opis: "WOLT", kategorija: "Wolt" },
-        { datum: "28.apr", tip: "isplata", iznos: 23500, opis: "Doplata za auto 200€", kategorija: "Ostale isplate" },
-        { datum: "STATUS", tip: "status", iznos: -6000, opis: "6.000 za majsku kiriju", kategorija: "Rezerva" }
-      ],
-      planiraniOstatak: 4275
-    },
-    {
-      mesec: "MAJ",
-      stavke: [
-        { datum: "1.maj", tip: "uplata", iznos: 41000, opis: "35.000 + 6.000 (iz rezerve)", kategorija: "Druge uplate" },
-        { datum: "1.maj", tip: "isplata", iznos: 41000, opis: "Kirija", kategorija: "Kirija" },
-        { datum: "13.maj", tip: "uplata", iznos: 21250, opis: "WOLT", kategorija: "Wolt" },
-        { datum: "15.maj", tip: "uplata", iznos: 46000, opis: "NBS", kategorija: "NBS" },
-        { datum: "15.maj", tip: "isplata", iznos: 17000, opis: "Kredit", kategorija: "Kredit" },
-        { datum: "15.maj", tip: "isplata", iznos: 27225, opis: "Računi", kategorija: "Računi" },
-        { datum: "28.maj", tip: "uplata", iznos: 21250, opis: "WOLT", kategorija: "Wolt" },
-        { datum: "28.maj", tip: "isplata", iznos: 35250, opis: "Dug 300€", kategorija: "Ostale isplate" },
-        { datum: "STATUS", tip: "status", iznos: -6000, opis: "6.000 za junsku kiriju", kategorija: "Rezerva" }
-      ],
-      planiraniOstatak: 7300
-    }
-  ];
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
